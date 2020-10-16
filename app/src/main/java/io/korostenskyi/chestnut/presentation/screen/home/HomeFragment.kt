001f76e617c3d2c02501b11b4b798955e0c071e5
Roman Korostenskyi <romankorostensky@gmail.com>
2020-10-16 16:59:10 +0300

    feature: home screen

---
+++ app/src/main/java/io/korostenskyi/chestnut/presentation/screen/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package io.korostenskyi.chestnut.presentation.screen.home
+
+import io.korostenskyi.chestnut.R
+import io.korostenskyi.chestnut.presentation.base.ui.BaseFragment
+
+class HomeFragment : BaseFragment(R.layout.fragment_home)
